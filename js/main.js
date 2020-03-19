31b3e769b1e2edb0413ada06b01ae823a84dc732
Scott Benson <scott.benson96@gmail.com>
2020-03-18 20:50:45 -0400

    corona message

--- js/main.js
+++ js/main.js
@@ -107,6 +107,14 @@ var resizeNavbar = function() {
 	}
 }
 
+	$(document).ready(function(){   
+    setTimeout(function () {
+        $("#cookieConsent").fadeIn(200);
+     }, 4000);
+    $("#closeCookieConsent, .cookieConsentOK").click(function() {
+        $("#cookieConsent").fadeOut(200);
+    }); 
+}); 
 
 	var siteMenuClone = function() {
 			$('.js-clone-nav').each(function() {
